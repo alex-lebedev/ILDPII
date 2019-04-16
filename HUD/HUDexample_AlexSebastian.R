b92fe596fc5f4bc34f598b5d636c576eb2cbc5ef
Alexander Lebedev <4508892+alex-lebedev@users.noreply.github.com>
2019-04-16 13:53:24 +0200

    Update HUDexample_AlexSebastian.R

--- HUD/HUDexample_AlexSebastian.R
+++ HUD/HUDexample_AlexSebastian.R
@@ -3,15 +3,16 @@
 #install.packages('jtools')
 #install.packages('broom')
 
+
 # Load libraries:
 library(reghelper)
 library(ggplot2)
 library(ggstance)
 library(radarchart)
+library(gtools)
 
 # Load data:
-load('/Users/alebedev/Documents/Projects/HUD/HUD.rda')
-load('/Users/alebedev/Documents/Projects/HUD/HUDMAIN_df.rda')
+load('/Users/alebedev/Documents/Projects/HUD/HUD_anonymized.rda')
 
 
 # Two-sample t-test:
@@ -21,9 +22,7 @@ boxplot(CONSP_df$CONS5[CONSP_df$group=='NP'],CONSP_df$CONS5[CONSP_df$group=='PP'
 points(cbind(jitter(rep(1, table(CONSP_df$group=='NP')[2])), CONSP_df$CONS5[CONSP_df$group=='NP']), pch=16)
 points(cbind(jitter(rep(2, table(CONSP_df$group=='PP')[2])), CONSP_df$CONS5[CONSP_df$group=='PP']), pch=16)
 
-fit <- (glm(CONS5~age+sex+ALC_prox+TOB_prox+CAN_prox+MDMA_prox+STIM_prox+OPI_prox+PSY_prox+
-                         ALC_freq+TOB_freq+CAN_freq+MDMA_freq+STIM_freq+OPI_freq+PSY_freq, data=CONSP_df))
-
+fit <- (glm(CONS5~age+sex+ALC_freqprox+TOB_freqprox+CAN_freqprox+MDMA_freqprox+STIM_freqprox+OPI_freqprox+PSY_prox, data=CONSP_df))
 plot_coefs(fit, scale = TRUE)
 
 # GLM
@@ -31,11 +30,6 @@ plot_coefs(fit, scale = TRUE)
 beta(glm(CONS5~PSY_prox, data=CONSP_df))
 beta(glm(CONS5~PSY_freq, data=CONSP_df))
 
-
-fit.full <- (glm(CONS5~age+sex+ALC_prox+TOB_prox+CAN_prox+MDMA_prox+STIM_prox+OPI_prox+PSY_prox+
-                         ALC_freq+TOB_freq+CAN_freq+MDMA_freq+STIM_freq+OPI_freq+PSY_freq, data=CONSP_df))
-
-plot_coefs(fit.full, scale = TRUE)
 
 
 # Radar plots:
