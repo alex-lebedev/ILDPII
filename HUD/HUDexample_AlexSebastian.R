3ed33ec0fa81cebee254c8a4fbafb231aa8e062e
Alexander Lebedev <4508892+alex-lebedev@users.noreply.github.com>
2019-04-17 13:11:56 +0200

    Update HUDexample_AlexSebastian.R

--- HUD/HUDexample_AlexSebastian.R
+++ HUD/HUDexample_AlexSebastian.R
@@ -2,7 +2,7 @@
 # Install packages:
 #install.packages('jtools')
 #install.packages('broom')
-
+#install.ackages('corrplot')
 
 # Load libraries:
 library(reghelper)
@@ -10,6 +10,7 @@ library(ggplot2)
 library(ggstance)
 library(radarchart)
 library(gtools)
+library(corrplot)
 
 # Load data:
 load('/Users/alebedev/Documents/Projects/HUD/HUD_anonymized.rda')
@@ -22,14 +23,20 @@ boxplot(CONSP_df$CONS5[CONSP_df$group=='NP'],CONSP_df$CONS5[CONSP_df$group=='PP'
 points(cbind(jitter(rep(1, table(CONSP_df$group=='NP')[2])), CONSP_df$CONS5[CONSP_df$group=='NP']), pch=16)
 points(cbind(jitter(rep(2, table(CONSP_df$group=='PP')[2])), CONSP_df$CONS5[CONSP_df$group=='PP']), pch=16)
 
-fit <- (glm(CONS5~age+sex+ALC_freqprox+TOB_freqprox+CAN_freqprox+MDMA_freqprox+STIM_freqprox+OPI_freqprox+PSY_prox, data=CONSP_df))
-plot_coefs(fit, scale = TRUE)
+# Main analysis:
+fitCONS5 <- (glm(CONS5~age+sex+ALC_freqprox+TOB_freqprox+CAN_freqprox+MDMA_freqprox+STIM_freqprox+OPI_freqprox+PSY_prox, data=CONSP_df))
+plot_coefs(fitCONS5, scale = TRUE)
 
-# GLM
-# standardized coefficients:
-beta(glm(CONS5~PSY_prox, data=CONSP_df))
-beta(glm(CONS5~PSY_freq, data=CONSP_df))
+# Follow-up analyses (each question):
+fitCONS_public <- (glm(CONS_public~age+sex+ALC_freqprox+TOB_freqprox+CAN_freqprox+MDMA_freqprox+STIM_freqprox+OPI_freqprox+PSY_prox, data=CONSP_df))
+fitCONS_polit <- (glm(CONS_polit~age+sex+ALC_freqprox+TOB_freqprox+CAN_freqprox+MDMA_freqprox+STIM_freqprox+OPI_freqprox+PSY_prox, data=CONSP_df))
+fitCONS_monit <- (glm(CONS_monit~age+sex+ALC_freqprox+TOB_freqprox+CAN_freqprox+MDMA_freqprox+STIM_freqprox+OPI_freqprox+PSY_prox, data=CONSP_df))
+fitCONS_connect <- (glm(CONS_connect~age+sex+ALC_freqprox+TOB_freqprox+CAN_freqprox+MDMA_freqprox+STIM_freqprox+OPI_freqprox+PSY_prox, data=CONSP_df))
+fitCONS_org <- (glm(CONS_org~age+sex+ALC_freqprox+TOB_freqprox+CAN_freqprox+MDMA_freqprox+STIM_freqprox+OPI_freqprox+PSY_prox, data=CONSP_df))
 
+# Check summaries:
+beta(fitCONS5)
+beta(fitCONS_public)
 
 
 # Radar plots:
@@ -45,3 +52,10 @@ chartJSRadar(scores=dfp, labelSize=20, scaleStartValue=15,height=700,
 # CMQ subscales:
 v='CONS_org'
 t.test(CONSP_df[CONSP_df$group=='NP',v],CONSP_df[CONSP_df$group=='PP',v])
+
+
+
+# Correlation plots:
+corrplot(cor(CONSP_df[,50:56]), col='black', method='number', type='lower')
+
+
