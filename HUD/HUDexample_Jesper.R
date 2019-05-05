a91fefa3efd2b4d38ff3d75a3de65844f676ea1b
Alexander Lebedev <4508892+alex-lebedev@users.noreply.github.com>
2019-05-05 20:50:56 +0200

    Update HUDexample_Jesper.R

--- HUD/HUDexample_Jesper.R
+++ HUD/HUDexample_Jesper.R
@@ -20,43 +20,46 @@ SCRFU_df_Jesper <- read.xlsx2('~/Downloads/HUD_anonymized_cleaned.xlsx',2)
 SCREEN_df_noPsych <- subset(SCREEN_df, SCREEN_df$PsychDiagAny==0)
 CONSP_df_noPsych <- subset(CONSP_df, CONSP_df$PsychDiagAny==0)
 
-dd <- merge(SCRFU_df,SCRFU_df_Jesper[,c('ID', 'surveyfoundout')], by = 'ID')
-        
-               
+dd <- merge(SCRFU_df,SCRFU_df_Jesper[,c('ID', 'surveyfoundout')], by = 'ID')      
+dd <- dd[!duplicated(dd$ID),]              
 # Visual
+dd$psyVisual[dd$psyVisual<0]<-0        
 table(dd$psyVisual>0)/sum(table(dd$psyVisual>0))
-table(dd$psyVisual==1)[2]/table(dd$psyVisual>0)[2]
-table(dd$psyVisual==2)[2]/table(dd$psyVisual>0)[2]
-table(dd$psyVisual==3)[2]/table(dd$psyVisual>0)[2]
-pie(table(dd$psyVisual))
-
+table(dd$psyVisual==1)[2]/sum(table(dd$psyVisual>0))
+table(dd$psyVisual==2)[2]/sum(table(dd$psyVisual>0))
+table(dd$psyVisual==3)[2]/sum(table(dd$psyVisual>0))
+pie(table(dd$psyVisual),labels =c(''), col=c('white', 'lightgrey','darkgrey','black'))
+        
 # Auditory:
+dd$psyAuditory[dd$psyAuditory<0]<-0
 table(dd$psyAuditory>0)/sum(table(dd$psyAuditory>0))
-table(dd$psyAuditory==1)[2]/table(dd$psyAuditory>0)[2]
-table(dd$psyAuditory==2)[2]/table(dd$psyAuditory>0)[2]
-table(dd$psyAuditory==3)[2]/table(dd$psyAuditory>0)[2]
-        
-dd <- subset(dd, dd$surveyfoundout!='HPPD Support Group')    
-        
+table(dd$psyAuditory==1)[2]/sum(table(dd$psyAuditory>0))
+table(dd$psyAuditory==2)[2]/sum(table(dd$psyAuditory>0))
+table(dd$psyAuditory==3)[2]/sum(table(dd$psyAuditory>0))
+pie(table(dd$psyAuditory),labels =c(''), col=c('white', 'lightgrey','darkgrey','black'))        
+#dd <- subset(dd, dd$surveyfoundout!='HPPD Support Group')         # then, do the same
 
         
-        
-        
-        
+# Designer drugs:
+dtypes <- c('PP', 'PP2')
+d1 <- read.xlsx2(paste('~/Downloads/',dtypes[1], '.xlsx',sep=''),2, stringsAsFactors=F)
+d2 <- read.xlsx2(paste('~/Downloads/',dtypes[2], '.xlsx',sep=''),2, stringsAsFactors=F)        
+dd1 <- data.frame(email=d1$VAR1, psyVisual = as.numeric(d1$VAR3_16), psyAuditory=as.numeric(d1$VAR3_17), desDrug1=NA,desDrug2=NA)
+dd2 <- data.frame(email=d2$VAR1, psyVisual = as.numeric(d2$VAR3_16), psyAuditory=as.numeric(d2$VAR3_17), desDrug1=d2$DP1, desDrug2=d2$DP2)
 
+ddrugs <- rbind(dd1,dd2)
+ddrugs <- ddrugs[!duplicated(ddrugs$email),]        
+ddrugs$psyVisual[ddrugs$psyVisual<0]<-0
+ddrugs$psyAuditory[ddrugs$psyAuditory<0]<-0
+
+# Proportions in relation to designer drug use:
         
-        
-        
-        # Auditory:
-table(dd$auditory>0)/sum(table(dd$auditory>0))
-table(dd$auditory==1)[2]/table(dd$auditory>0)[2]
-table(dd$auditory==2)[2]/table(dd$auditory>0)[2]
-table(dd$auditory==3)[2]/table(dd$auditory>0)[2]
-      
-        
-        
-        
-        
+table(ddrugs$psyVisual[ddrugs$desDrug1==1]>0)/sum(table(ddrugs$psyVisual[ddrugs$desDrug1==1]>0))
+table(ddrugs$psyVisual[ddrugs$desDrug1==2]>0)/sum(table(ddrugs$psyVisual[ddrugs$desDrug1==2]>0))
+
+table(ddrugs$psyAuditory[ddrugs$desDrug1==1]>0)/sum(table(ddrugs$psyAuditory[ddrugs$desDrug1==1]>0))                                                          
+table(ddrugs$psyAuditory[ddrugs$desDrug1==2]>0)/sum(table(ddrugs$psyAuditory[ddrugs$desDrug1==2]>0))        
+
         
         
 # DOTS:
